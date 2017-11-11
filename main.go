25581d59c61a93347fc0c0f06f0710a9ba26b53c
Yuya Moriguchi <laprasdrum@c-fo.com>
2017-11-11 12:19:50 +0900

    Use go-homedir

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/user"
 	"strconv"
 	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
 	"github.com/shurcooL/githubql"
 	"github.com/urfave/cli"
 	"golang.org/x/oauth2"
@@ -90,8 +90,11 @@ var (
 )
 
 func main() {
-	user, _ := user.Current()
-	tomlData = user.HomeDir + "/.config/peekabow/config.toml"
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+	tomlData = home + "/.config/peekabow/config.toml"
 
 	app := cli.NewApp()
 	app.Name = appName
